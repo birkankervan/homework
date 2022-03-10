176059d7a872c4a56564bbb9b989d37097fa8542
Emre Birkan Kervan <kervan95@gmail.com>
2022-03-10 19:17:29 +0300

    search id added

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import teamMembers from '../data/data.js'
+
+const PORT = 3000
+const app = express()
+
+app.get('/team', (req, res) => {
+    res.send(teamMembers)
+})
+
+app.get('/team/:id', (req, res) => {
+    const id = req.params.id
+    const searchResult = teamMembers.find((member) => member.id === +id)
+    if (searchResult) {
+        res.send(JSON.stringify(searchResult))
+    } else {
+        res.status(404).send('ID not found.')
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
