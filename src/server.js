1d7197ab70805763851eec73c3b0ce00ac5c0411
Emre Birkan Kervan <kervan95@gmail.com>
2022-03-18 11:23:36 +0300

    lint test

--- src/server.js
+++ src/server.js
@@ -5,19 +5,19 @@ const PORT = 3000
 const app = express()
 
 app.get('/team', (req, res) => {
-    res.send(teamMembers)
+        res.send(teamMembers)
 })
 
 app.get('/team/:id', (req, res) => {
-    const id = req.params.id
-    const searchResult = teamMembers.find((member) => member.id === +id)
-    if (searchResult) {
-        res.send(searchResult)
-    } else {
-        res.status(404).send('ID not found.')
-    }
+        const id = req.params.id
+        const searchResult = teamMembers.find((member) => member.id === +id)
+        if (searchResult) {
+                res.send(searchResult)
+        } else {
+                res.status(404).send('ID not found.')
+        }
 })
 
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
+        console.log(`listening on port ${PORT}`)
 })
